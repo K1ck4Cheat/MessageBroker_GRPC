a9842027158300b0f627d47e4a1a755a9711e32e
Gore Aurelian <44786945+K1ck4Cheat@users.noreply.github.com>
2021-10-10 13:52:50 +0300

    Add files via upload

---
+++ Broker/Models/Message.cs
@@ -0,0 +1,20 @@
+
+
+
+namespace Broker.Models 
+{
+
+     public class Message
+     {
+          public Message(string topic, string content)
+          {
+               Topic = topic;
+               Content = content;
+          }
+
+          public string Topic { get; }
+
+          public string Content { get; }
+     }
+
+}
